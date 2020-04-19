07563e6aaeb730d0fafa91d1a5d4e5015cd1d0ce
yami-er <queenyani@gmail.com>
2020-04-19 17:11:30 -0400

    second commit

--- index.js
+++ index.js
@@ -3,20 +3,27 @@ function convertToFahranite(K){
     return F.toPrecision(2)
 }
 $("button#1").click(function(){
-    var zipcode = $("#zip").val();
+  var zipcode = $("#zip").val(); 
+  // var currentTime = moment().format('h:mm:ss a');
+  // $("#weatherid1").text("Local time:"+currentTime) 
+  
+  var weathereApikey="34c9a261402ccf438cebedb1dee0a341"
+  var timeApiKey="3HCA50JZWG8Q"
+  
+  var weatherApi = "http://api.openweathermap.org/data/2.5/weather?"+"zip="+zipcode+",us"+"&appid="+weathereApikey;
+  $.get(weatherApi,function(result){
+    var weather = ("Weather: "+" " + result.weather[0].main);
+    var temp = ("Temprature: "+" " +convertToFahranite(result.main.temp));
+    var temp2 = ("Feels like: "+" " +convertToFahranite(result.main.feels_like));
+    $("#weatherid1").text(weather) 
+    $("#weatherid2").text(temp+ " " +"F")
+    $("#weatherid3").text(temp2+ " " +"F")
     
-    var myAppid="34c9a261402ccf438cebedb1dee0a341"
-    
-    var weatherApi = "http://api.openweathermap.org/data/2.5/weather?"+"zip="+zipcode+",us"+"&appid="+myAppid;
-    
-    
-	$.get(weatherApi,function(result){
-         var desc = ("Clouds: "+" " + result.weather[0].description);
-         var temp = ("Temprature: "+" " +convertToFahranite(result.main.temp));
-         var win = ("Wind speed: "+" " + result.wind.speed);
-         $ ("#weatherid1").text(desc)
-         $ ("#weatherid2").text(temp)
-         $ ("#weatherid3").text(win)
-    })
-    
+    var lon = result.coord.lon;
+    var lat = result.coord.lat;
+    var timeApi = "http://api.timezonedb.com/v2.1/get-time-zone?key="+timeApiKey+"&format=json&by=position&lat="+lat+"&lng="+lon
+    $.get(timeApi,function(result){
+          $("#weatherid4").text("Time:"+moment(result.formatted).format('h:mm:ss a'));
+      })
   })
+}) 
